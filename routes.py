6ebd2328bb2d1b2060abb1634dbe4c6ce2379ff0
Roman Zhernosek <ipshot007@gmail.com>
2017-02-16 22:06:38 +0300

    define routes methods

--- routes.py
+++ routes.py
@@ -2,15 +2,15 @@ from flask import Flask
 app = Flask(__name__)
 
 # Taxi routes
-@app.route('/taxi/release')
-def handle_taxi_release():
+@app.route('/taxi/release', methods=['POST', 'PUT'])
+def handle_release_taxi():
   return ''
 
 # Passenger routes
-@app.route('/passenger/order/create')
+@app.route('/passenger/order/create', methods=['POST', 'PUT'])
 def handle_create_order():
 	return ''
 
-@app.route('/passenger/order/cancel')
+@app.route('/passenger/order/cancel', methods=['POST', 'PUT'])
 def handle_cancel_order():
 	return ''
