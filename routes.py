6e19bb74127ed249b1fac0b6bdd1557c742bf49f
Roman Zhernosek <ipshot007@gmail.com>
2017-02-16 21:58:00 +0300

    define routes template

--- routes.py
+++ routes.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+# Taxi routes
+@app.route('/taxi/release')
+def handle_taxi_release():
+  return ''
+
+# Passenger routes
+@app.route('/passenger/order/create')
+	def handle_create_order():
+		return ''
+
+@app.route('/passenger/order/cancel')
+	def handle_cancel_order():
+		return ''
