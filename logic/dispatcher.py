50e4a198a5a82b6f0211173e56593a3ee2fc677c
Roman Zhernosek <ipshot007@gmail.com>
2017-02-17 19:39:53 +0300

    added order cancel, added maps

--- logic/dispatcher.py
+++ logic/dispatcher.py
@@ -2,41 +2,62 @@ class Dispatcher:
 	def __init__(self):
 		self.free_cars = []
 		self.orders = []
+		self.orders_map = {}
+		self.free_cars_map = {}
 
 	def release_car(self, car):
+		id = car.get_id()
 		if car.is_valid():
 			self.free_cars.append(car)
+			self.free_cars_map[id] = len(self.free_cars)
 			msg = (
-				'Taxi id=' + car.get_id() + 
+				'Taxi id=' + id + 
 				': was released successfully'
 			)
 			print(msg)
 			return msg, 200
 		else:
 			msg = (
-				'Taxi id=' + car.get_id() + 
+				'Taxi id=' + id + 
 				': release car request data is invalid'
 			)
 			print(msg)
 			return msg, 400
 
 	def create_order(self, passenger):
+		id = passenger.get_id()
 		if passenger.is_valid():
 			self.orders.append(passenger)
+			self.orders_map[id] = len(self.orders)
 			msg = (
-				'Passenger id=' + passenger.get_id() + 
+				'Passenger id=' + id + 
 				': order was created successfully'
 			)
 			print(msg)
 			return msg, 200
 		else:
 			msg = (
-				'Passenger id=' + passenger.get_id() + 
+				'Passenger id=' + id + 
 				': create order request data is invalid'
 			)
 			print(msg)
 			return msg, 400
-		pass
 
 	def cancel_order(self, passenger):
-		pass
+		id = passenger.get_id()
+		if passenger.is_valid():
+			idx = self.orders_map[id]
+			del self.orders[idx]
+			msg = (
+				'Passenger id=' + id + 
+				': order was canceld successfully'
+			)
+			print(msg)
+			return msg, 200
+		else:
+			msg = (
+				'Passenger id=' + id + 
+				': cancel order request data is invalid'
+			)
+			print(msg)
+			return msg, 400
--- tests/test.py
+++ tests/test.py
@@ -3,12 +3,14 @@ from time import time
 
 def send_post(url, data):
 	try:
-		requests.post(url, data)
+		print(requests.post(url, data).text)
 	except requests.exceptions.ConnectionError:
 		print('Connection error. May be you forgot to start server?')
+		exit()
 	except requests.exceptions.RequestException as e:
 		print('Error while trying to send POST %s', url)
 		print(e)
+		exit()
 
 # Release taxi post request
 send_post(
